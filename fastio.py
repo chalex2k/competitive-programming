3bb6c316b059e5d8f2073864d06b5216ef0982ef
chalex2k <chalex2k@gmail.com>
2025-01-01 18:00:04 +0300

    Быстрый ввод и вывод

---
+++ fastio.py
@@ -0,0 +1,26 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+YES = 'YES'
+NO = 'NO'
+
+
+def print(*args, sep=' ', end='\n'):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def si():  # string input
+    return input()
+
+
+def ii():  # int input
+    return int(input())
+
+
+def lsi():  # list string input
+    return input().split()
+
+
+def lii():  # list int input
+    return list(map(int, input().split()))
